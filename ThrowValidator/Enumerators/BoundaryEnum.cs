902312d564a166409b29c7ef204bc0d243d08e44
Sachintha Silva <netsachintha@gmail.com>
2022-06-12 12:40:15 +0530

    Add code documentation for Boundary enumerator

--- ThrowValidator/Enumerators/BoundaryEnum.cs
+++ ThrowValidator/Enumerators/BoundaryEnum.cs
@@ -1,7 +1,25 @@
-﻿public enum Boundary
+﻿/// <summary>
+/// Boundary restriction rules when comparing a value with respect to left (lower-bound) and right (upper-bound) margins.
+/// </summary>
+public enum Boundary
 {
+    /// <summary>
+    /// Include both left and right margins while evaluating the range.
+    /// </summary>
     Inclusive,
+
+    /// <summary>
+    /// Exclude both left and right margins while evaluating the range.
+    /// </summary>
     Exclusive,
+
+    /// <summary>
+    /// Include only left but not the right margins while evaluating the range.
+    /// </summary>
     LeftOnly,
+
+    /// <summary>
+    /// Include only right but not the left margins while evaluating the range.
+    /// </summary>
     RightOnly
 }
--- ThrowValidator/Exceptions/ConditionNotMeetException.cs
+++ ThrowValidator/Exceptions/ConditionNotMeetException.cs
@@ -2,18 +2,33 @@
 
 namespace ThrowValidator.Exceptions
 {
+    /// <summary>
+    /// Exception when the conditinal predicate does not meet.
+    /// </summary>
     public class ConditionNotMeetException : Exception
     {
+        /// <summary>
+        /// Initialize an instance of <see cref="ConditionNotMeetException"/>.
+        /// </summary>
         public ConditionNotMeetException()
         {
 
         }
 
+        /// <summary>
+        /// Initialize an instance of <see cref="ConditionNotMeetException"/> with specified <paramref name="message"/>.
+        /// </summary>
+        /// <param name="message">The error message that explains the reason for this exception.</param>
         public ConditionNotMeetException(string message) : base(message)
         {
 
         }
 
+        /// <summary>
+        /// Initialize an instance of <see cref="ConditionNotMeetException"/> with specified <paramref name="message"/> and <paramref name="inner"/> exception that cause this exception.
+        /// </summary>
+        /// <param name="message">The error message that explains the reason for this exception.</param>
+        /// <param name="inner">The exception that is the cause of the current exception, or a null reference.</param>
         public ConditionNotMeetException(string message, Exception inner) : base(message, inner)
         {
 
